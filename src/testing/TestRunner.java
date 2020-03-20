23bdbac900016d91048723b298cf77b6143118cb
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-20 10:58:08 +0530

    Update TestRunner.java

--- src/testing/TestRunner.java
+++ src/testing/TestRunner.java
@@ -1,4 +1,5 @@
 package testing;
+/* Uncomment the below code to test your application
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -15,3 +16,4 @@ public class TestRunner {
 		System.out.println(result.wasSuccessful());
 	}
 }
+*/
