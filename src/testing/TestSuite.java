b6f966f35f5bfb7c415d2206beaa6e7d8a54a29c
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-20 10:59:46 +0530

    Update TestSuite.java

--- src/testing/TestSuite.java
+++ src/testing/TestSuite.java
@@ -1,5 +1,7 @@
 package testing;
 
+/* Uncomment the below code to test your application
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -9,3 +11,4 @@ import org.junit.runners.Suite.SuiteClasses;
 public class TestSuite {
 
 }
+*/
